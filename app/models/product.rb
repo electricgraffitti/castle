a22a933940cfb18d5912633cb7bcf4d298436ea1
Larry FInn <Cube1@larry-finns-macbook-pro-3.local>
2011-04-30 14:21:19 -0700

    nothing here

--- app/models/product.rb
+++ app/models/product.rb
@@ -14,6 +14,7 @@
 #  list_order          :integer(4)
 #  dependent_item      :boolean(1)
 #  interactive_service :boolean(1)
+#  requires_location   :boolean(1)
 #
 
 class Product < ActiveRecord::Base
